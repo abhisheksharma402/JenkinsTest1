416d9b09ce14504efda18a69cdf9b25db8198c87
Abhishek Sharma <51467874+abhisheksharma402@users.noreply.github.com>
2024-02-06 00:50:49 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from prog import summation
+
+class TestSum(unittest.TestCase):
+  def test_list_int(self):
+    data=[23,32]
+    result = summation(data)
+    self.assertEqual(result,55)
+
+
+if __name__ == '__main__':
+  unittest.main()
